3544dccec9b33c52b7c106abdbe53f16649bec24
ASLIDDINCODER <147050428+ASLIDDINCODERn1@users.noreply.github.com>
2024-04-25 21:22:14 +0500

    first commit

---
+++ lib/bank_card.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class MyBankCard extends StatefulWidget {
+  const MyBankCard({super.key});
+
+  @override
+  State<MyBankCard> createState() => _MyBankCardState();
+}
+
+class _MyBankCardState extends State<MyBankCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: Padding(
+          padding: EdgeInsets.all(23),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(child: ,)
+              
+            ],
+          )),
+    );
+  }
+}
